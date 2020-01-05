27d8eac9b12cad3e85120ecaed9ba9e13c5ac599
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-05 03:44:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,11 +8,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language
-  
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.js."
 end
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
   
 end
 
